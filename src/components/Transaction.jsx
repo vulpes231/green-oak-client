11dfa8dc710b23ade8ec261be39118b296b40657
Vulpes231 <whykayshot@gmail.com>
2023-12-15 11:36:32 +0100

    fix-styling

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Transaction = ({ title, date, amount }) => {
   return (
     <div className="flex justify-between border-b py-4">
-      <span>
-        <h3 className="uppercase font-semibold">{title}</h3>
+      <span className="w-[70%]">
+        <h3 className="uppercase font-light text-xs md:text-sm">{title}</h3>
         <p className="font-extralight">{date}</p>
       </span>
-      <span className="font-semibold">{amount}</span>
+      <span className=" whitespace-nowrap text-xs md:text-sm w-[30%]flex justify-end font-light">
+        {amount}
+      </span>
     </div>
   );
 };
